818cce64d9bbe6ff23cc8a4f5f45eb0eb6febc73
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-08 13:43:26 -0600

    Add pages

---
+++ API/client/src/components/RandomTweetPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RandomTweetPage() {
+  return (
+    <main>Random Tweet Page</main>
+  )
+}
+
+export default RandomTweetPage;
---
+++ API/client/src/components/TweetSearchPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TweetSearchPage() {
+  return (
+    <main>Tweet Search Page</main>
+  )
+}
+
+export default TweetSearchPage;
