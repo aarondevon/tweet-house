0026ecc85c600c2ca7a99c3a590e63d01b62f12e
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-08 13:49:24 -0600

    Set up client side routing

--- API/client/src/App.js
+++ API/client/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import RandomTweetPage from './components/RandomTweetPage';
+import TweetSearchPage from './components/TweetSearchPage';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-          <Header />
-      </div>
+      <Router>
 
+        <div className="container">
+          <Header />
+        </div>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+          <Route path="/random-tweet">
+            <RandomTweetPage />
+          </Route>
+          <Route path="/tweet-search">
+            <TweetSearchPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- API/client/src/components/Nav.jsx
+++ API/client/src/components/Nav.jsx
@@ -1,17 +1,18 @@
 ﻿import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
     return (
       <nav>
         <ul className="nav">
           <li className="nav-item">
-            <a className="nav-link active" href="#">Home</a>
+            <Link className="nav-link active" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Random Tweet</a>
+            <Link className="nav-link" to="/random-tweet">Random Tweet</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Tweet Search</a>
+            <Link className="nav-link" to="tweet-search">Tweet Search</Link>
           </li>
         </ul>
       </nav>
