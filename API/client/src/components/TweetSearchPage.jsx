62b220ead7982fad6fd8e1b074c43757553cbe61
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-09 10:04:56 -0600

    Apply eslint rules

--- API/client/src/components/TweetSearchPage.jsx
+++ API/client/src/components/TweetSearchPage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function TweetSearchPage() {
   return (
     <main>Tweet Search Page</main>
-  )
+  );
 }
 
 export default TweetSearchPage;
