7c79733f03697311c23c4c9e2169e4e95b22fca5
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-11 10:33:08 -0600

    Remove test tweets

--- API/client/src/components/RandomTweetPage.jsx
+++ API/client/src/components/RandomTweetPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Tweet from './Tweet';
 
 function RandomTweetPage() {
   return (
-    <main>Random Tweet Page</main>
+    <main id="random-tweet-page" className="flex center-y column">
+      <Tweet />
+      <Tweet />
+      <Tweet />
+      <Tweet />
+      <Tweet />
+    </main>
   );
 }
 
