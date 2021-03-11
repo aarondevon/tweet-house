c40a2ead3f449d6f8e7809a75941e171dba0883f
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-11 10:33:32 -0600

    Create Tweet component

---
+++ API/client/src/components/Tweet.jsx
@@ -0,0 +1,29 @@
+// eslint-disable react/destructuring-assignment
+import React from 'react';
+
+function Tweet(props) {
+  return (
+    // style="width: 18rem;"
+    <div className="card">
+      <div className="row">
+        <div className="col-1">
+          <img className="profile-picture" src={props.profileImage} alt="twitter profile" />
+        </div>
+        <div className="col-11">
+          <div className="row">
+            <div className="col-12">
+              <div className="card-body">
+                <h5 className="card-title">{props.screenName}</h5>
+                <p className="card-text">
+                  {props.tweetText}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tweet;
