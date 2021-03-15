7802b647e9e83d32d34de60bb2b748090082771d
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-15 15:01:43 -0500

    Create tweet and user model

---
+++ TwitterShowcaseLibrary/Models/TweetModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterShowcaseLibrary.Models
+{
+    public class TweetModel
+    {
+        public string full_text { get; set; }
+        public int[] display_text_range { get; set; }
+        public string source { get; set; }
+        public TwitterUserModel user { get; set; }
+        public int retweet_count { get; set; }
+        public int favorite_count { get; set; }
+    }
+}
---
+++ TwitterShowcaseLibrary/Models/TwitterUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitterShowcaseLibrary.Models
+{
+    public class TwitterUserModel
+    {
+        public int id { get; set; }
+        public string id_str { get; set; }
+        public string name { get; set; }
+        public string screen_name { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string profile_background_color { get; set; }
+        public string profile_background_image_url { get; set; }
+        public string profile_background_image_url_https { get; set; }
+        public string profile_image_url { get; set; }
+        public string profile_image_url_https { get; set; }
+        public string profile_banner_url { get; set; }
+    }
+}
