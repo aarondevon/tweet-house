fc619a7f719b37ce01fd9913058342e3a4e1a76a
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-22 17:14:32 -0500

    Get specific user

---
+++ TwitterShowcaseLibrary/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterShowcaseLibrary.Models;
+
+namespace TwitterShowcaseLibrary.Services
+{
+    public interface IUserService
+    {
+        Task<TwitterUserModel> GetUserData(string bearerToken, string screenName);
+    }
+}
---
+++ TwitterShowcaseLibrary/Twitter/IGetUserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterShowcaseLibrary.Models;
+
+namespace TwitterShowcaseLibrary.Twitter
+{
+    public interface IGetUserData
+    {
+        Task<TwitterUserModel> ReturnUserData(string bearerToken, string screenName);
+    }
+}
