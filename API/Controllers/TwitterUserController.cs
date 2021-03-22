4908a0553bfcd2742b9f081ec86c9efa77add69e
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-22 14:12:51 -0500

    Conditionally use bearer token

--- API/Controllers/TwitterUserController.cs
+++ API/Controllers/TwitterUserController.cs
@@ -24,9 +24,15 @@ namespace API.Controllers
 
         public async Task<IActionResult> getUserTweet(string user)
         {
-            // Test deploy
-            //string bearerToken = _configuration.GetValue<string>("TwitterSettings:BearerToken");
-            string bearerToken = Environment.GetEnvironmentVariable("TWITTER_BEARER_TOKEN");
+            string bearerToken;
+            if (_configuration.GetValue<bool>("inDevelopment"))
+            {
+                bearerToken = _configuration.GetValue<string>("TwitterSettings:BearerToken");
+            }
+            else
+            {
+                bearerToken = Environment.GetEnvironmentVariable("TWITTER_BEARER_TOKEN");
+            }
 
             var result = await _userTweet.GetTweetDataBasedOnUser(bearerToken, user);
 
