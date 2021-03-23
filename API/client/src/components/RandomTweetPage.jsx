e1ba216f7ad7c622411226c825134868346d11d1
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-23 11:15:45 -0500

    Get profile data and display it

--- API/client/src/components/RandomTweetPage.jsx
+++ API/client/src/components/RandomTweetPage.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+/* eslint-disable arrow-body-style */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import UserCard from './UserCard';
-import Tweet from './Tweet';
+// import Tweet from './Tweet';
 
 function RandomTweetPage() {
+  const [screenNames] = useState(['npmjs', 'freeCodeCamp', 'ClassicalMPR', 'BBCScienceNews', 'NASA']);
+  const [userProfileData, setUserProfileData] = useState([]);
+
+  const getUserProfileData = async (screenName) => {
+    const response = await axios.get(`https://localhost:44322/api/twitter-user/${screenName}`);
+    return response.data;
+  };
+
+  const setUserProfileDataToState = async () => {
+    const profileData = screenNames.map((screenName) => {
+      return getUserProfileData(screenName);
+    });
+    console.log(profileData);
+    setUserProfileData(await Promise.all(profileData));
+  };
+
+  const getRandomTweetOnClick = () => {
+
+  };
+
+  const displayUsers = () => {
+    return userProfileData.map((userData) => {
+      return (
+        <UserCard
+          userData={userData}
+          getRandomTweet={getRandomTweetOnClick}
+        />
+      );
+    });
+  };
+
+  useEffect(() => {
+    setUserProfileDataToState();
+  }, []);
+
   return (
     <main id="random-tweet-page" className="flex center-y column">
-      <UserCard />
-      {/* <Tweet
-        profileImage={tweet.profileImage}
-        screenName={tweet.screenName}
-        tweetText={tweet.tweetText}
-      /> */}
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      {displayUsers()}
     </main>
   );
 }
--- API/client/src/components/UserCard.jsx
+++ API/client/src/components/UserCard.jsx
@@ -1,23 +1,25 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 
-function UserCard() {
+function UserCard(props) {
   return (
     <div className="card">
       <div className="row">
-        <div className="col-6">
-          <img className="profile-picture" src={props.profileImage} alt="twitter profile" />
+        <div className="col-3">
+          <img className="profile-picture" src={props.userData.profileImageUrl} alt="twitter profile" />
         </div>
-        <div className="col-6">
+        <div className="col-9">
           <div className="row">
             <div className="col-12">
               <div className="card-body">
-                <h5 className="card-title">{props.screenName}</h5>
-                <p className="card-text">
-                  {props.name}
-                </p>
-                <button className="btn btn-primary">View Tweet</button>
+                <h5 className="card-title">{props.userData.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {`@${props.userData.screenName}`}
+                  {console.log(props)}
+                </h6>
+                <button className="btn btn-primary" onClick={props.getRandomTweet}>View Tweet</button>
               </div>
             </div>
           </div>
