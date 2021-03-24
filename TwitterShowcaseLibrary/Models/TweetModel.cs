19d39b4d5e775b15294070a778f21631bbbfd108
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-24 10:40:06 -0500

    update prop names

--- TwitterShowcaseLibrary/Models/TweetModel.cs
+++ TwitterShowcaseLibrary/Models/TweetModel.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TwitterShowcaseLibrary.Models
 {
+    [DataContract]
     public class TweetModel
     {
-        public string full_text { get; set; }
-        public int[] display_text_range { get; set; }
-        public string source { get; set; }
-        public TwitterUserModel user { get; set; }
-        public int retweet_count { get; set; }
-        public int favorite_count { get; set; }
+        [DataMember(Name = "full_text")]
+        public string FullText { get; set; }
+        [DataMember(Name = "display_text_range")]
+        public int[] DisplayTextRange { get; set; }
+        [DataMember(Name = "source")]
+        public string Source { get; set; }
+        [DataMember(Name = "user")]
+        public TwitterUserModel User { get; set; }
+        [DataMember(Name = "retweet_count")]
+        public int RetweetCount { get; set; }
+        [DataMember(Name = "favorite_count")]
+        public int FavoriteCount { get; set; }
     }
 }
