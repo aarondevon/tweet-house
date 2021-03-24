40934bc349b583cb7b10de646e10fb22bbbd142d
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-24 10:41:08 -0500

    reference tweet data

--- API/client/src/components/Tweet.jsx
+++ API/client/src/components/Tweet.jsx
@@ -8,6 +8,7 @@ function Tweet(props) {
     <div className="card">
       <div className="row">
         <div className="col-1">
+          {console.log(props)}
           <img className="profile-picture" src={props.tweetData.user.profileImageUrl} alt="twitter profile" />
         </div>
         <div className="col-11">
@@ -15,8 +16,11 @@ function Tweet(props) {
             <div className="col-12">
               <div className="card-body">
                 <h5 className="card-title">{props.tweetData.user.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {`@${props.tweetData.user.screenName}`}
+                </h6>
                 <p className="card-text">
-                  {props.full_text}
+                  {props.tweetData.fullText}
                 </p>
               </div>
             </div>
