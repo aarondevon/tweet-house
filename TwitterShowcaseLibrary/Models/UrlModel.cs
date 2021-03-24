99c9f19e95985d69dc0d3011927b44fedea88b0b
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-24 14:51:01 -0500

    Create additional models

---
+++ TwitterShowcaseLibrary/Models/UrlModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitterShowcaseLibrary.Models
+{
+    public class UrlModel
+    {
+        public string url { get; set; }
+        public string expanded_url { get; set; }
+        public string display_url { get; set; }
+    }
+
+}
