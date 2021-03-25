5274391ee5e3efbd19c8a7ef83991bbeda456aa0
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-03-25 12:53:37 -0500

    Update content

--- API/client/src/components/Home.jsx
+++ API/client/src/components/Home.jsx
@@ -11,12 +11,12 @@ function Home() {
           <div className="col-sm-12 col-md-6">
             <h1 className="display-4">Welcome!</h1>
             <p className="lead">
-              Twitter Showcase uses the Twitter API, React, and C#. C#
+              Tweet House uses the Twitter API, React, and C#. C#
               handles the data from the Twitter API and React interacts via the API created
               with C#. The app allows the user to generate a random tweet from my favorite
               Twitter users. Twitter Showcase also allows the user to search for a Twitter
-              user and display the 5-10 most recent tweets by that user, or the user can
-              search for specific content and have the 5-10 most recent tweets display that
+              user and display some of the most recent tweets by that user, or the user can
+              search for specific content and have up to 25 of the most recent tweets display that
               contain that content.
             </p>
           </div>
